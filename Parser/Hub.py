32ef6ed193d816811138ae539b378e244217ebb6
Alexandre da silva peroba <asilvaperoba@gmail.com>
2026-06-01 18:56:16 -0300

    convensão de tupla

--- Parser/Hub.py
+++ Parser/Hub.py
@@ -8,8 +8,7 @@ class hub:
 			dict_keys = list(self.metadata.keys())
 			
 			if dict_keys[0] != "start_hub":
-				print("[Error]: Invalid metadata keys")
-				return
+				raise ValueError("[Error]: Invalid metadata keys")
 
 			dict_values = list(self.metadata.values())
 			dados = tuple(dict_values[0].split(","))
@@ -17,8 +16,15 @@ class hub:
 			#tratamento para verificar se existe caracter invalido no nome
 			for i in dados[0]:
 				if (i == "-" or i == " "):
-					print("[Error]: Name invalid")
-					return
+					raise ValueError("[Error]: Name invalid")
+			
+			#tratamento para verificar se existe valores invalidos
+			try:
+				dados[1] = int(dados[1]) if isinstance(dados[1], str) else dados[1]
+				dados[2] = int(dados[2]) if isinstance(dados[2], str) else dados[2]
+			except ValueError:
+				raise ValueError("[Error]: Invalid data types")
+			
 			
 			
 
